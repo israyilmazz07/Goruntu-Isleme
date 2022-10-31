0d67148ca6845dfe8c644513ba6206ca09a06ad6
israyilmazz07 <iylmaz4423@gmail.com>
2022-10-31 12:15:42 +0300

    .
    .

--- pythonOdev2/main.py
+++ pythonOdev2/main.py
@@ -4,14 +4,17 @@ import numpy as np
 kus=cv2.imread("kus.jpg",0)
 cv2.imshow("kus.jpg",kus)
 cv2.waitKey()
-max= (np.max(kus) - np.min(kus))/(np.max(kus) + np.min(kus)) # maxımum değeri bulur.
+
+max = np.max(kus)
+print(max)
 
 [l,m] = kus.shape
-new_kus = np.zeros([l,m], dtype=np.uint8)
+newkus = np.zeros([l,m], dtype=np.uint8)
+
 for i in range(l):
     for j in range(m):
-        new_kus[i,j] = max - kus[i,j]  #maxımum değerden görüntüyü çıkartıp görüntünün ters görünmesini sağlıyo.
+        newkus[i,j] = max - kus[i,j]  #maxımum değerden görüntüyü çıkartıp görüntünün ters görünmesini sağlıyo.
 
 
-cv2.imshow("Manuel_inverted",new_kus)
+cv2.imshow("Negatif Kus",newkus)
 cv2.waitKey()
